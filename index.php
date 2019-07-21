44e24f0fd8724c9b1321dcf74a32e95a0c7ae4f8
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-07-21 19:58:53 +1000

    Make Netbeans Project + Get index.php from main FTP server

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Welcome to The Thing Doers Website</title>
+<meta name="description" content="This website exists as a proof of concept for an Automated Clinical Coding System">
+<meta name="keywords" content="clinical coding system, proof of concept">
+<meta name="author" content="Brendan, Callum, Jess, Joe, Michael, Won">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<style type="text/css">
+<!--
+body			{margin:0; background-color:#eee;}
+header, nav, main, footer {max-width:960px; margin:0 auto 0 auto;}
+header, footer		{height:200px; background-color:#555; color:#fff;}
+main			{padding:25px 0 25px 0; background-color:#fff;}
+nav			{height:40px; line-height:40px; background-color:#ccc;}
+
+h1, h2, h3		{margin:0; font-family:"Times New Roman", Times, serif;}
+h1			{padding:0px 10px 0px 10px;}
+h2, h3			{padding:15px 25px 0px 25px;}
+h3			{font-style:italic}
+header h1		{text-align:center;}
+nav li, p, footer li	{margin:0; font-family: Arial, Helvetica, sans-serif;}
+p			{padding:10px 2em 0px 2em; text-align:justify}
+ul			{margin:0; padding:0;}
+nav ul, footer ul	{list-style:none;}
+nav li			{display:inline-block; width:48%; text-align:center;}
+footer ul		{text-align:center; line-height:200px;}
+li a			{color:#fff; text-decoration:none; font-weight:bold}
+a:hover			{text-decoration:underline;}
+-->
+</style>
+</head>
+<body>
+<header>
+  <h1><br><br>The Thing Doers</h1>
+</header>
+<nav>
+  <ul>
+    <li><a href="login.php">Clinical Coding System</a></li>
+    <li><a href="about.php">About Us</a></li>
+  </ul>
+</nav>
+<main>
+  <h2>Into to the Clinical Coding System</h2>
+  <h3>Forward</h3>
+  <p>This group project is building on the initial project idea developed by Brendan and available for viewing here: https://brendan6780.github.io/ProjectIdea.html. Further adaptations by ‘The Thing Doers’, such as incorporation of additional functionalities and production of a UI wireframe, are discussed in the  section ‘Further Considerations for Development by The Thing Doers’.</p>
+  <br>
+  <h3>Background</h3>
+  <p>Public hospitals in Queensland and other Australian states receive funding from Medicare and other government bodies on a per-patient-per-procedure basis (QLD Health 2018). For funding to be allocated correctly to each hospital, the procedures undertaken on a per-patient basis must be recorded and remitted to Medicare or the relevant funding body in a timely fashion. The current procedure for recording and remitting this information, called clinical coding, is extremely manual and time-consuming. This task involves an administrative worker reviewing patient charts and practitioner/nurse notes to determine what the medical condition was, which procedures were performed, and what materials were used in the process. Each condition and procedure is then assigned a code based on the Australian clinical coding standard ICD-10-AM; for example, P59.9 Jaundice in newborn (IHPA 2019). At times critical information such as the condition or procedure can be up to interpretation, due to complexity or inability to decipher clinician handwriting.</p>
+  <p>Where small hospitals may be able to keep up with procedures using this manual method, large hospitals require a clinical coding team to process the many procedures performed daily. This project proposes an automated system for clinical coding, whereby clinicians enter procedure details directly into a patient’s electronic chart (where in practice by the hospital) or into a stand-alone application, either of which will automatically code said procedure and produce a summary of codes ready for remittal to Medicare. This system will reduce time spent reviewing patient charts and prevent miscoding due misinterpreted practitioner notes.</p>
+</main>
+<footer>
+  <ul>
+    <li><a href="mailto:s3807909@student.rmit.edu">Contact us</a></li>
+  </ul>
+</footer>
+</body>
+</html>
