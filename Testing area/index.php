c4fd28bf56f6b3697f43459d46114bdcfb214be7
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-07-24 10:23:11 +1000

    Testing area index

---
+++ Testing area/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test hub</title>
+    </head>
+    <body>
+        <a href="./RepeaterSetup.php"> POST repeater </a>
+        <br>
+        <a href="./test.php"> Datepicker test </a>
+    </body>
+</html>
