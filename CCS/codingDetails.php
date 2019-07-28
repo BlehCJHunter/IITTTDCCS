c144f9de0a1041b5a71806b9b502bea5bbda90ca
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-07-28 20:07:24 +1000

    Finally stopped the login bug Turns out strcompare() crashes. Also, syncronised with website again.

---
+++ CCS/codingDetails.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Clinical Coder v0.1</title>
+        <meta name="description" content="This website exists as a proof of concept for an Automated Clinical Coding System">
+        <meta name="keywords" content="clinical coding system, proof of concept">
+        <meta name="author" content="Brendan, Callum, Jess, Joe, Michael, Won">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="format-detection" content="telephone=no">
+        <link href="https://fonts.googleapis.com/css?family=Heebo:300&display=swap" rel="stylesheet"> 
+        <style type="text/css">
+            <!--
+            html, body		{margin:0 .7em; font-family: 'Heebo', sans-serif;}
+            header, footer	{text-align:center;}
+            header			{font-weight:bold; color:#444; background-color:#eee;}
+            main			{margin:3em auto 3em auto; background-color:#fff}
+            .barcode		{margin:5px; padding:9px 7px; font-size:0.7em; color:#fff; border:1px solid #007FFF; border-radius:6px; background-color:#007FFF;}
+            table			{max-width:960px; margin:3em auto 3em auto;}
+            td				{display:table-cell; padding-top: 0.65em;}
+            label			{font-size:0.65em; font-weight:bold; color:#666;}
+            .titleCell		{text-align:right}
+            .infoCell		{text-align:center; font-size:0.8em;}
+            .dataEntry		{padding-left:1.5em; line-height:1.6em;}
+            #extraInfo		{vertical-align:middle; padding-left:1em; line-height:1.6em; font-size:0.6em;}
+            textarea		{width:99%; height:75px;}
+            footer			{padding-right:10px; text-align:right;}
+            -->
+        </style>
+    </head>
+    <body>
+        <header>
+            Jane Doe | UR 123 45 67 890 | DOB 05APR1984
+        </header>
+        <main>
+            <form action="somewhere.php" method="POST">
+                <table>
+
+                    <tr>
+
+                        <td class="titleCell"><label for="assessmentDate">Date of assessment:</label></td>
+                        <td class="dataEntry"><input class="field" name="assessmentDate" type="text" min="2" max="25"></td>
+                    </tr><tr>
+                        <td class="titleCell"><label for="diagnosis1">Diagnosis[1]:</label></td>
+                        <td class="dataEntry"><input class="field" name="diagnosis1" type="text" min="2" max="25"></td>
+                    </tr><tr>
+                        <td class="titleCell"><label for="procedure1">Procedure[1]:</label></td>
+                        <td class="dataEntry"><input class="field" name="procedure1" type="text" min="2" max="25"></td>
+                    </tr><tr>
+                        <td class="titleCell"><label for="additional">Additional Materials:</label></td>
+                        <td class="dataEntry"><textarea></textarea></td>
+		</tr><tr>
+			<td class="titleCell"><label for="status">Patient Status:</label></td>
+			<td class="dataEntry">
+				<select name="status">
+					<option value="patient1">Alive</option>
+					<option value="patient2">Injured</option>
+					<option value="patient3">Call insurance!</option>
+				</select>
+			</td>
+		</tr><tr>
+			<td class="titleCell"><label for="address">Notes for clinical<br>coding team:</label></td>
+			<td class="dataEntry"><textarea></textarea></td>
+		</tr><tr>
+			<td colspan="2" class="infoCell">
+				<input class="resetButton" type="reset" value="Reset">
+				<input class="confirmButton" name="login" type="submit" value="Confirm">
+			</td>
+		
+		</tr>
+
+	</table>
+                </form>
+</main>
+	<footer>
+		<form action="index.php" method="post">
+			<input type="hidden" value="logout" name="logout" />
+			<input class="button" type="submit" value="Logout">
+		</form>
+	</footer>
+</body>
+</html>
--- CCS/login.php
+++ CCS/login.php
@@ -1,117 +1,132 @@
-<?php include 'admin/includes/dbconnect.inc';?>
+<?php include 'admin/includes/dbconnect.inc'; ?>
 <?php
 session_start();
+$debug = 1;
 if (!strlen(@$_POST["usr"]) < 2) { //Sanity check to enable easter egg
-    $usr = htmlspecialchars(stripslashes(@$_POST["usr"])); //Make sure we don't get 1337 h4x0r'd on.
+    $usr = htmlspecialchars(stripslashes($_POST["usr"])); //Make sure we don't get 1337 h4x0r'd on.
+    $nousr = 0;
 } else {
-    $usr = htmlspecialchars(stripslashes(@$_POST["pain_is_weakness"])); //Memes
+    $usr = ""; //Memes
+    $nousr = 1;
 }
-if(!strlen(@$_POST["pwd"]) < 8){
-        $pwd = htmlspecialchars(stripslashes(@$_POST["pwd"])); //These are expected to not exist at first
-} else{
-        $pwd = htmlspecialchars(stripslashes(@$_POST["leaving_the_body"])); //Memes 2: I went too far
+if (!strlen(@$_POST["pwd"]) < 8) {
+    $pwd = htmlspecialchars(stripslashes($_POST["pwd"]));
+    $nopwd = 0;
+} else {
+    $pwd = "";
+    $nopwd = 1; //Memes 2: I went too far
 }
-$query = "SELECT `Password Token` FROM Users WHERE `Username`='Bleh'";
+$query = "SELECT `Password Token` FROM Users WHERE `Username`='" . $usr . "'";
 $key = mysqli_fetch_array(mysqli_query($conn, $query));
 $conn->close();
 $userkey = hash("sha512", $usr . "ZYX" . $pwd . "ABC");
-$active = @$_SESSION['active'];
+$active = $_SESSION['active'];
+if ($debug == 1) {
+    echo "Key State: " . $key . "<br>";
+    echo "Key: " . $key["Password Token"] . "<br>";
+    echo "User Key: " . $userkey . "<br>";
+    echo "Session State: " . @$_SESSION['active'] . "<br>";
+    debug_print_backtrace();
+}
 if (!empty($active)) {
     if ($active >= "1") {
-        header("Location: ../index.php");
+        if ($debug == 1) {
+            session_unset();
+        }
+        header("Location: index.php");
         exit();
     } else if ($active == "0") {
-        if (!empty($usr) && !empty($pwd)) {
-            if (strcomp($userkey, $key["Password Token"]) == 0) {
+        if (!$nousr && !$nopwd) {
+            if ($userkey == $key["Password Token"]) {
                 $_SESSION['active'] = "1";
-                header("Location: ../index.php");
+                header("Location: index.php");
                 exit();
             } else {
                 $message = "<span style='color:red'>The username or password was incorrect</span>";
             }
         } else {
-            $message = "<span style='color:red'>You went to great lengths to not input your " . (!empty($usr) ? "password" : "username") . "</span>";
+            $message = "<span style='color:red'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
         }
     }
 } else {
-    if (empty($usr) && empty($pwd)) {
+    if ($nousr && $nopwd) {
         $message = "&nbsp;";
     } else {
-        if (!empty($usr) && !empty($pwd)) {
-            if (strcomp($userkey, $key["Password Token"]) == 0) {
+        if (!$nousr && !$nopwd) {
+            if ($userkey == $key["Password Token"]) {
                 $_SESSION['active'] = "1";
-                header("Location: ../index.php");
+                header("Location: index.php");
                 exit();
             } else {
                 $message = "<span style='color:red'>The username or password was incorrect</span>";
             }
         } else {
-            $message = "<span style='color:red'>You went to great lengths to not input your " . (!empty($usr) ? "password" : "username") . "</span>";
+            $message = "<span style='color:red'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
         }
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Clinical Coder v0.1</title>
-<meta name="description" content="This website exists as a proof of concept for an Automated Clinical Coding System">
-<meta name="keywords" content="clinical coding system, proof of concept">
-<meta name="author" content="Brendan, Callum, Jess, Joe, Michael, Won">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="format-detection" content="telephone=no">
-<link href="https://fonts.googleapis.com/css?family=Heebo:300&display=swap" rel="stylesheet"> 
-<style type="text/css">
-<!--
-html, body	{margin:0; height:100%; font-family: 'Heebo', sans-serif;}
-main		{display:table; position:absolute; top:0; left:0; height:100%; width:100%;}
-header		{position:absolute; width:100%; height:40px; text-align:center; line-height:40px; font-weight:bold; color:#444; background-color:#eee; z-index:2}
-#inner		{display:table-cell; vertical-align:middle}
-main form	{margin-left:auto; margin-right:auto; width:275px; text-align:center; color:#666;}
-#topBorder, #bottomBorder	{width:275px;}
-#topBorder	{background-color:#fafafa; border:1px solid #bbb; border-bottom:0px; border-radius:15px 15px 0px 0px;}
-#bottomBorder	{line-height:35px; border:1px solid #bbb; border-radius:0px 0px 15px 15px;}
-#formTitle	{font-size:1.1em; font-weight:bold;}
-.field		{width:200px; height:23px; border:1px solid #aaa; border-radius:3px;}
-.button		{margin:5px; padding:4px 13px; font-size:1.02em; color:#007FFF; background-color:#fff; border:none; border-radius:5px}
-#space		{line-height:8px;}
-#leftAlignBlock	{display:block; margin-left:auto; margin-right:auto; width:200px; text-align:left;}
-#tiny		{margin-top:15px; line-height:16px; font-size:0.7em;}
--->
-</style>
-</head>
-<body>
-<header>
-	Clinical Coder v0.1
-</header>
-<main>
-  <div id="topBar"></div>
-  <div id="inner">
-    <form action="login.php" method="POST">
-      <div id="topBorder">
-	<div id="space">&nbsp;</div>
-	<div id="space">&nbsp;</div>
-	<div id="formTitle">
-	  Please Sign In<br>
-	</div>
-	<div id="tiny">Please sign in using your [Health Department]<br>credentials to access this system.</div>
-	<br>
-	<div id="leftAlignBlock">
-	  <label for="username">Username:</label><br>
-	  <input class="field" name="usr" type="text" min="2" max="25" required autofocus>
-	  <div id="space">&nbsp;</div>
-	  <label for="password">Password:</label><br>
-	  <input class="field" name="pwd" type="password" min="8" max="50" required>
-	</div>
-	  <br><?php echo $message; ?><br><br>
-	</div>
-	<div id="bottomBorder">
-	  <input class="button" type="reset" value="Reset">
-	  <input class="button" name="login" type="submit" value="Login">
-	</div>
-    </form>
-  </div>
-</main>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Clinical Coder v0.1</title>
+        <meta name="description" content="This website exists as a proof of concept for an Automated Clinical Coding System">
+        <meta name="keywords" content="clinical coding system, proof of concept">
+        <meta name="author" content="Brendan, Callum, Jess, Joe, Michael, Won">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="format-detection" content="telephone=no">
+        <link href="https://fonts.googleapis.com/css?family=Heebo:300&display=swap" rel="stylesheet"> 
+        <style type="text/css">
+            <!--
+            html, body	{margin:0; height:100%; font-family: 'Heebo', sans-serif;}
+            main		{display:table; position:absolute; top:0; left:0; height:100%; width:100%;}
+            header		{position:absolute; width:100%; height:40px; text-align:center; line-height:40px; font-weight:bold; color:#444; background-color:#eee; z-index:2}
+            #inner		{display:table-cell; vertical-align:middle}
+            main form	{margin-left:auto; margin-right:auto; width:275px; text-align:center; color:#666;}
+            #topBorder, #bottomBorder	{width:275px;}
+            #topBorder	{background-color:#fafafa; border:1px solid #bbb; border-bottom:0px; border-radius:15px 15px 0px 0px;}
+            #bottomBorder	{line-height:35px; border:1px solid #bbb; border-radius:0px 0px 15px 15px;}
+            #formTitle	{font-size:1.1em; font-weight:bold;}
+            .field		{width:200px; height:23px; border:1px solid #aaa; border-radius:3px;}
+            .button		{margin:5px; padding:4px 13px; font-size:1.02em; color:#007FFF; background-color:#fff; border:none; border-radius:5px}
+            #space		{line-height:8px;}
+            #leftAlignBlock	{display:block; margin-left:auto; margin-right:auto; width:200px; text-align:left;}
+            #tiny		{margin-top:15px; line-height:16px; font-size:0.7em;}
+            -->
+        </style>
+    </head>
+    <body>
+        <header>
+            Clinical Coder v0.1
+        </header>
+        <main>
+            <div id="topBar"></div>
+            <div id="inner">
+                <form action="login.php" method="POST">
+                    <div id="topBorder">
+                        <div id="space">&nbsp;</div>
+                        <div id="space">&nbsp;</div>
+                        <div id="formTitle">
+                            Please Sign In<br>
+                        </div>
+                        <div id="tiny">Please sign in using your [Health Department]<br>credentials to access this system.</div>
+                        <br>
+                        <div id="leftAlignBlock">
+                            <label for="username">Username:</label><br>
+                            <input class="field" name="usr" type="text" max="25" required autofocus>
+                            <div id="space">&nbsp;</div>
+                            <label for="password">Password:</label><br>
+                            <input class="field" name="pwd" type="password" max="50" required>
+                        </div>
+                        <br><?php echo $message; ?><br><br>
+                    </div>
+                    <div id="bottomBorder">
+                        <input class="button" type="reset" value="Reset">
+                        <input class="button" name="login" type="submit" value="Login">
+                    </div>
+                </form>
+            </div>
+        </main>
+    </body>
 </html>
