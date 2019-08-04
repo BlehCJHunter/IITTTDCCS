215c76c5e2934f45cb13dcabcd4dde107e7c2be0
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-08-04 19:17:38 +1000

    Commit Mike's changes to the login script Albeit with a properly closed else statement

--- CCS/login.php
+++ CCS/login.php
@@ -1,79 +1,94 @@
 <?php
 session_start();
-$debug = 0; // Echo keys etc etc, and destroys the session when revisiting
-if ($_SESSION['active']) {
-    if ($debug == 1) {
-        session_unset();
+
+$debug = 1; // Echo keys etc etc, and destroys the session when revisiting
+if ($debug == 1) {
+    if ($_SESSION['active']) {
+        header("Location: https://ccs.geekrunner.net/CCS/index.php");
     }
-    header("Location: https://ccs.geekrunner.net/CCS/index.php");
+    session_destroy();
 }
-if (strlen(htmlspecialchars(stripslashes($_POST["usr"]))) > 0) { //Sanity check to enable easter egg
-    $usr = htmlspecialchars(stripslashes($_POST["usr"])); //Make sure we don't get 1337 h4x0r'd on.
-    $nousr = 0;
-} else {
-    $usr = ""; //Memes
-    $nousr = 0;
-}
-if (strlen(htmlspecialchars(stripslashes($_POST["pwd"]))) > 0) {
-    $pwd = htmlspecialchars(stripslashes($_POST["pwd"]));
-    $nopwd = 0;
-} else {
-    $pwd = "";
-    $nopwd = 0; //Memes 2: I went too far
-}
-if (!$nousr && !$nopwd) {
-    require 'admin/includes/dbconnect.inc';
-    $query = "SELECT `Password Token` FROM Users WHERE `Username`='" . $usr . "'";
-    $key = mysqli_fetch_array(mysqli_query($conn, $query));
-    if ($debug) {
-        echo mysqli_info($conn);
-    }
-    $conn->close();
-}
-$userkey = hash("sha512", $usr . "ZYX" . $pwd . "ABC");
-$active = $_SESSION['active'];
-if ($debug) {
-    echo "Key State: " . $key . "<br>";
-    echo "Key: " . $key["Password Token"] . "<br>";
-    echo "User Key: " . $userkey . "<br>";
-    echo "Session State: " . @$_SESSION['active'] . "<br>";
-    echo "User: " . $usr . "<br>";
-    echo "Pwd: " . $pwd . "<br>";
-    debug_print_backtrace();
-}
-if (!empty($active)) {
-    if ($active >= "1") {
-        header("Location: index.php");
-        exit();
-    } else if ($active == "0") {
-        if (!$nousr && !$nopwd) {
-            if ($userkey == $key["Password Token"]) {
-                $_SESSION['active'] = "1";
-                $_SESSION['key'] = $key["Password Token"];
-                header("Location: index.php");
-                exit();
-            } else {
-                $message = "<span style='color:red'>The username or password was incorrect</span>";
-            }
-        } else {
-            $message = "<span style='color:red'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
-        }
-    }
-} else {
-    if ($nousr && $nopwd) {
+
+$usr = $_POST["usr"];
+$pwd = $_POST["pwd"];
+if (!empty($usr) xor ! empty($pwd)) {
+    if (empty($usr) && empty($pwd)) {
         $message = "&nbsp;";
+    } else if (empty($usr)) {
+        $message = "Username is missing";
     } else {
-        if (!$nousr && !$nopwd) {
-            if ($userkey == $key["Password Token"]) {
-                $_SESSION['active'] = "1";
-                $_SESSION['key'] = $key["Password Token"];
-                header("Location: index.php");
-                exit();
+        $message = "Password is missing";
+    }
+} else {
+
+    if (strlen(htmlspecialchars(stripslashes($_POST["usr"]))) > 0) { //Sanity check to enable easter egg
+        $usr = htmlspecialchars(stripslashes($_POST["usr"])); //Make sure we don't get 1337 h4x0r'd on.
+        $nousr = 0;
+    } else {
+        $usr = ""; //Memes
+        $nousr = 0;
+    }
+    if (strlen(htmlspecialchars(stripslashes($_POST["pwd"]))) > 0) {
+        $pwd = htmlspecialchars(stripslashes($_POST["pwd"]));
+        $nopwd = 0;
+    } else {
+        $pwd = "";
+        $nopwd = 0; //Memes 2: I went too far
+    }
+    if (!$nousr && !$nopwd) {
+        require 'admin/includes/dbconnect.inc';
+        $query = "SELECT `Password Token` FROM Users WHERE `Username`='" . $usr . "'";
+        $key = mysqli_fetch_array(mysqli_query($conn, $query));
+        if ($debug) {
+            echo mysqli_info($conn);
+        }
+        $conn->close();
+    }
+    $userkey = hash("sha512", $usr . "ZYX" . $pwd . "ABC");
+    $active = $_SESSION['active'];
+    if ($debug) {
+        echo "Key State: " . $key . "<br>";
+        echo "Key: " . $key["Password Token"] . "<br>";
+        echo "User Key: " . $userkey . "<br>";
+        echo "Session State: " . @$_SESSION['active'] . "<br>";
+        echo "User: " . $usr . "<br>";
+        echo "Pwd: " . $pwd . "<br>";
+        debug_print_backtrace();
+    }
+    if (!empty($active)) {
+        if ($active >= "1") {
+            header("Location: index.php");
+            exit();
+        } else if ($active == "0") {
+            if (!$nousr && !$nopwd) {
+                if ($userkey == $key["Password Token"]) {
+                    $_SESSION['active'] = "1";
+                    $_SESSION['key'] = $key["Password Token"];
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    $message = "<span class='message'>The username or password was incorrect</span>";
+                }
             } else {
-                $message = "<span style='color:red'>The username or password was incorrect</span>";
+                $message = "<span class='message'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
             }
+        }
+    } else {
+        if (empty($_SESSION['usr']) && empty($_SESSION['pwd'])) {
+            $message = "&nbsp;";
         } else {
-            $message = "<span style='color:red'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
+            if (!$nousr && !$nopwd) {
+                if ($userkey == $key["Password Token"]) {
+                    $_SESSION['active'] = "1";
+                    $_SESSION['key'] = $key["Password Token"];
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    $message = "<span class='message'>The username or password was incorrect</span>";
+                }
+            } else {
+                $message = "<span class='message'>You went to great lengths to not input your " . (!$nousr ? "password" : "username") . "</span>";
+            }
         }
     }
 }
@@ -88,24 +103,58 @@ if (!empty($active)) {
         <meta name="author" content="Brendan, Callum, Jess, Joe, Michael, Won">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="format-detection" content="telephone=no">
-        <link href="https://fonts.googleapis.com/css?family=Heebo:300&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/baseStyle.css">
+        <link href="https://fonts.googleapis.com/css?family=Heebo:300&display=swap" rel="stylesheet">
         <style type="text/css">
-            <!--
-            html, body	{margin:0; height:100%; font-family: 'Heebo', sans-serif;}
-            main		{display:table; position:absolute; top:0; left:0; height:100%; width:100%;}
-            header		{position:absolute; width:100%; height:40px; text-align:center; line-height:40px; font-weight:bold; color:#444; background-color:#eee; z-index:2}
-            #inner		{display:table-cell; vertical-align:middle}
-            main form	{margin-left:auto; margin-right:auto; width:275px; text-align:center; color:#666;}
-            #topBorder, #bottomBorder	{width:275px;}
-            #topBorder	{background-color:#fafafa; border:1px solid #bbb; border-bottom:0px; border-radius:15px 15px 0px 0px;}
-            #bottomBorder	{line-height:35px; border:1px solid #bbb; border-radius:0px 0px 15px 15px;}
-            #formTitle	{font-size:1.1em; font-weight:bold;}
-            .field		{width:200px; height:23px; border:1px solid #aaa; border-radius:3px;}
-            .button		{margin:5px; padding:4px 13px; font-size:1.02em; color:#007FFF; background-color:#fff; border:none; border-radius:5px}
-            #space		{line-height:8px;}
-            #leftAlignBlock	{display:block; margin-left:auto; margin-right:auto; width:200px; text-align:left;}
-            #tiny		{margin-top:15px; line-height:16px; font-size:0.7em;}
-            -->
+            html, body {
+                height:100%;}
+            header {
+                position:absolute;
+                width:100%;
+                height:40px;
+                text-align:center;
+                line-height:40px;
+                font-weight:bold;
+                color:#444;
+                background-color:#eee;
+                z-index:2;
+            }
+            main {
+                display:table;
+                position:absolute;
+                left:0;
+                height:100%;
+                width:100%;
+            }
+            main form {
+                margin-left:auto;
+                margin-right:auto;
+                width:275px;
+                text-align:center;
+            }
+            #topBorder {
+                background-color:#fafafa;
+                border:1px solid #bbb;
+                border-bottom:0px;
+                border-radius:15px 15px 0px 0px;
+            }
+            #inner {
+                display:table-cell; vertical-align:middle
+            }
+            #bottomBorder {
+                line-height:35px;
+                border:1px solid #bbb;
+                border-radius:0px 0px 15px 15px;
+            }
+            .field {
+                width:200px;
+                height:23px;
+                border:1px solid #aaa;
+                border-radius:3px;
+            }
+            #space {
+                line-height:15px;
+            }
         </style>
     </head>
     <body>
@@ -113,11 +162,9 @@ if (!empty($active)) {
             Clinical Coder v0.1
         </header>
         <main>
-            <div id="topBar"></div>
             <div id="inner">
                 <form action="login.php" method="POST">
                     <div id="topBorder">
-                        <div id="space">&nbsp;</div>
                         <div id="space">&nbsp;</div>
                         <div id="formTitle">
                             Please Sign In<br>
