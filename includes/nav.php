a2f31af3017392d35603f31cbafd4d458e2f6438
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-08-11 15:49:00 +1000

    Really minor change to nav.php Mike, you're up

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,11 @@
-	<nav>
-		<ul>
-			<li><a href="CCS/login.php">Clinical Coding System</a></li>
-			<li><a href="Testing area/index.php">Testing area</a></li>
-			<li><a href="Testing area/testForJoe.php">Test for Joe</a></li>
-			<li><a href="about.php">About Us</a></li>
-		</ul>
-	</nav>
+<?php
+$WhereTheHeckOurAboutPageIs = "about.php"; //Mike, this is yours to deal with
+?>
+<nav>
+    <ul>
+        <li><a href="CCS/login.php">Clinical Coding System</a></li>
+        <li><a href="Testing area/index.php">Testing area</a></li>
+        <li><a href="Testing area/testForJoe.php">Test for Joe</a></li>
+        <li><a href="<?php echo $WhereTheHeckOurAboutPageIs; ?>">About Us</a></li>
+    </ul>
+</nav>
