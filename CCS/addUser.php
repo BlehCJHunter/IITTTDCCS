6c7a79cfcba40752a674cb4a01151f70ed6fa0ea
BlehCJHunter <s3804323@student.rmit.edu.au>
2019-08-16 16:23:00 +1000

    User Search/Update functionality Thank goodness for copy/paste

--- CCS/addUser.php
+++ CCS/addUser.php
@@ -8,11 +8,11 @@ foreach ($_POST as $key => $value) {
 }
 switch ($uType) {
     case "patient":
-        if($search == "Search"){
+        if ($search == "Search") {
             $target = "Location: index.php?AUI=4&searchfor=" . $Medno;
             header($target);
             exit();
-        } 
+        }
         if (!$delete && ($_SESSION['access'] == 3 || $_SESSION['access'] == 4 || $_SESSION['dbg'])) {
             require 'admin/includes/dbconnect.inc';
             $AAAAAAA = mysqli_query($conn, ($add ? "SELECT MAX(`Patient ID`) FROM `Patient_Details`" : "SELECT * FROM `Patient_Details`"));
@@ -48,7 +48,7 @@ switch ($uType) {
             $query .= "`Medicare No.` = '";
             $query .= $Medno . "'";
             if (mysqli_query($conn, $query)) {
-                echo "Record " . ($add ? "created" : "updated") . " successfully";
+                echo "Record deleted successfully";
             } else {
                 echo "Error: " . $query . "<br>" . mysqli_error($conn);
                 $conn->close();
@@ -59,22 +59,43 @@ switch ($uType) {
         break;
 
     case "user":
-        if ($_SESSION['access'] == 4 || $_SESSION['dbg']) {
+        if ($search == "Search") {
+            $target = "Location: index.php?AUI=4&usearchfor=" . $uName;
+            header($target);
+            exit();
+        }
+        if (!$delete && ($_SESSION['access'] == 4 || $_SESSION['dbg'])) {
             require 'admin/includes/dbconnect.inc';
             $AAAAAAA = mysqli_query($conn, "SELECT MAX(`User ID`) FROM `Users`"); // I SCREAM. This is the new User ID
             $AAAAAA = mysqli_fetch_row($AAAAAAA); // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0x41414141414141
             $query = "";
-            $query .= $add ? "INSERT INTO `Users` VALUES ('" : "UPDATE `Users` ";
-            $query .= ($AAAAAA[0] + 1) . "', '";
-            $query .= $fName . "', '";
-            $query .= $lName . "', '";
-            $query .= $title . "', '";
-            $query .= $acc . "', '";
-            $query .= $uName . "', '";
-            $query .= hash("sha512", $uName . "ZYX" . $pWord . "ABC") . "', '";
-            $query .= date("Y-m-d H:i:s", time()) . "', '";
-            $query .= $email . "', '";
-            $query .= $phone . "')";
+            $query .= $add ? "INSERT INTO `Users` VALUES ('" : "UPDATE `Users` SET ";
+            $query .= $add ? ($AAAAAA[0] + 1) . "', " : "";
+            $query .= ($add ? "'" : "`First Name` = '") . $fName . "', ";
+            $query .= ($add ? "'" : "`Last Name` = '") . $lName . "', ";
+            $query .= ($add ? "'" : "`Position Title` = '") . $title . "', ";
+            $query .= ($add ? "'" : "`Access_ID` = '") . $acc . "', ";
+            $query .= ($add ? "'" : "`Username` = '") . $uName . "', ";
+            $query .= ($add ? "'" . hash("sha512", $uName . "ZYX" . $pWord . "ABC") . "', " : (strlen($pWord) > 1 ? "`Password Token` = '" . hash("sha512", $uName . "ZYX" . $pWord . "ABC") . "', " : ""));
+            $query .= ($add ? "'" . date("Y-m-d H:i:s", time()) . "', " : "");
+            $query .= ($add ? "'" : "`Email` = '") . $email . "', ";
+            $query .= ($add ? "'" : "`Phone Number` = '") . $phone . "'";
+            $query .= ($add ? ") " : " WHERE ");
+            $query .= ($add ? "" : "`User ID` = '" . $UID . "'");
+            if (mysqli_query($conn, $query)) {
+                echo "Record " . ($add ? "created" : "updated") . " successfully";
+            } else {
+                echo "Error: " . $query . "<br>" . mysqli_error($conn);
+                $conn->close();
+                exit();
+            }
+            $conn->close();
+        } elseif ($delete) {
+            require 'admin/includes/dbconnect.inc';
+            $query = "";
+            $query .= "DELETE FROM `Patient_Details` WHERE ";
+            $query .= "`Username` = '";
+            $query .= $uName . "'";
             if (mysqli_query($conn, $query)) {
                 echo "Record deleted successfully";
             } else {
